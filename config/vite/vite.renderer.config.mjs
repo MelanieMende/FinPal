149dd6de2e38590ecf0fc57e23c5ff13ab2d852b
Melanie Mende <melanie.mende@me.com>
2025-12-10 09:28:41 +0100

    tailwind

---
+++ config/vite/vite.renderer.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    nodePolyfills(),
+    tailwindcss(),
+  ],
+})
